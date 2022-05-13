4fbf3ed958fdf5becae89096f65d5a1f78a0df67
Spazi <talishmarprkle+g@gmail.com>
2022-05-13 08:42:33 +0300

    Generic sounds, particle hit test, playmode is faster, ignored layout .dwlt and vscode settings

---
+++ Assets/scripts/IDamageable.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+
+//This is a generic interface where T is a placeholder
+//for a data type that will be provided by the 
+//implementing class.
+public interface IDamageable<T>
+{
+    void Damage(T hitPosition);
+}
